cb91164bc83519a75f1e75cbdd6989fc10c18f9c
Jorge Mina <minajorge@hotmail.com>
2021-02-01 12:11:47 -0500

    Initial commit

---
+++ src/main/kotlin/com/cdmtransformadores/produccion/ProduccionApplication.kt
@@ -0,0 +1,11 @@
+package com.cdmtransformadores.produccion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProduccionApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProduccionApplication>(*args)
+}
---
+++ src/test/kotlin/com/cdmtransformadores/produccion/ProduccionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cdmtransformadores.produccion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProduccionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
