bffe5a9664b3e31f9c94706ce7c78ba5d43ab504
Jorge Mina <minajorge@hotmail.com>
2021-02-05 07:19:00 -0500

    Update design class

--- src/main/kotlin/com/cdmtransformadores/produccion/models/Design.kt
+++ src/main/kotlin/com/cdmtransformadores/produccion/models/Design.kt
@@ -13,37 +13,140 @@ import javax.validation.constraints.NotNull
 class Design {
     @Id
     @NotBlank
-    var id: String?=null
+    var id: String? = null
+
+    @NotNull
+    @Column
+    var kva: Int? = null
 
     @Column
     @NotBlank
-    var description:String?=null
+    var marca: String? = null
 
     @Column
     @NotNull
-    var fecha: LocalDate?=null
+    var fecha: LocalDate? = null
 
     @NotNull
     @Column
-    var kva: Int?=null
+    var fase: Int? = null
 
     @NotNull
     @Column
-    var fase: Int?=null
+    var voltajeNominalPrimario: Int? = null
 
     @NotNull
     @Column
-    var peso: Int?=null
+    var voltajeNominalSecundario: Int? = null
+
+    @Column
+    @NotBlank
+    var tipoDeNucleo: String? = null
+
+    @Column
+    @NotBlank
+    var formaDelNucleo: String? = null
+
+    @Column
+    @NotNull
+    var cantidadDeFe: Int? = null
+
+    @Column
+    @NotNull
+    var anchoDeFe: Int? = null
+
+    @Column
+    @NotNull
+    var anchoVentana: Int? = null
+
+    @Column
+    @NotNull
+    var altoVentana: Int? = null
+
+    @Column
+    @NotBlank
+    var conductorSecundario: String? = null
+
+    @Column
+    @NotBlank
+    var grupoConnection: String? = null
+
+    @Column
+    @NotBlank
+    var tipoLaminaMagnetic: String? = null
+
+    @Column
+    @NotNull
+    var positionNominalConmutador: Int? = null
 
     @NotNull
     @Column
-    var cantidadAceite: Int?=null
+    var pesoNucleo1: Double? = null
+
+    @Column
+    var pesoNucleo2: Double? = null
 
     @NotNull
     @Column
-    var tipoAceite: String?=null
+    var pesoTotal: Double? = null
+
+    @Column
+    @NotBlank
+    var poW: Int? = null
+
+    @Column
+    @NotBlank
+    var induction: String? = null
+
+    @Column
+    @NotNull
+    var inductionTDesign: Double? = null
+
+    @Column
+    @NotBlank
+    var connectionBajaTension: String? = null
+
+    @Column
+    @NotBlank
+    var connectionAltaTension: String? = null
+
+    @Column
+    @NotNull
+    var lmn1: Double? = null
+
+    @Column
+    @NotNull
+    var lmn2: Double? = null
+
+    @Column
+    @NotNull
+    var anet: Double? = null
+
+    @Column
+    @NotNull
+    var wkgCalculado: Double? = null
+
+    @Column
+    @NotNull
+    var wkgTablaLamina: Double? = null
+
+    @Column
+    @NotNull
+    var inductionTablaLamina: Double? = null
+
+    @Column
+    @NotNull
+    var inductionIngresoManual: Double? = null
+
+    @NotNull
+    @Column
+    var cantidadAceite: Int? = null
+
+    @NotNull
+    @Column
+    var tipoAceite: String? = null
 
     @NotBlank
     @Column
-    var tipoTransformador: String?=null
+    var tipoTransformador: String? = null
 }
