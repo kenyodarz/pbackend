ffb677f9242fafe54fb9564167989685e435667e
Jorge Mina <minajorge@hotmail.com>
2021-02-20 07:27:36 -0500

    Actualizada La Consulta

--- src/main/kotlin/com/cdmtransformadores/produccion/repositories/ModeloRepository.kt
+++ src/main/kotlin/com/cdmtransformadores/produccion/repositories/ModeloRepository.kt
@@ -7,7 +7,14 @@ import org.springframework.data.jpa.repository.Query
 interface ModeloRepository : JpaRepository<Modelo, String> {
 
     @Query(
-        value = "SELECT m.id_modelo, m.cantidad_aceite, m.fase, m.kva, m.nombre_modelo, m.peso, m.tipo_aceite, m.tipo_transformador, m.design_id, count(t) as stock from modelos m \nJOIN transformadores t ON m.id_modelo = t.modelo_id_modelo \nWHERE t.cliente = 'CDM' \nGROUP BY m.id_modelo",
+        value = """ 
+            SELECT m.id_modelo, m.cantidad_aceite, m.fase, m.kva, m.nombre_modelo, m.peso, m.tipo_aceite, m.tipo_transformador, m.design_id, 
+                count(t) AS stock
+            FROM modelos m
+            JOIN transformadores t ON m.id_modelo = t.modelo_id_modelo AND t.in_production = false
+            WHERE t.cliente = 'CDM'
+            GROUP BY m.id_modelo
+            """,
         nativeQuery = true
     )
     fun getAllWithStock(): List<Modelo>
