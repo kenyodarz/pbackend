718f3358a70db55cff939c8d6d8fd5d5dc71d851
Jorge Mina <minajorge@hotmail.com>
2021-02-26 13:50:10 -0500

    Modificada la consulta SQL para retornar 0 en caso de no haber stock

--- src/main/kotlin/com/cdmtransformadores/produccion/repositories/ModeloRepository.kt
+++ src/main/kotlin/com/cdmtransformadores/produccion/repositories/ModeloRepository.kt
@@ -8,12 +8,16 @@ import java.util.*
 interface ModeloRepository : JpaRepository<Modelo, String> {
 
     @Query(
-        value = """SELECT m.id_modelo, m.cantidad_aceite, m.fase, m.kva, m.nombre_modelo, m.peso, m.tipo_aceite, m.tipo_transformador, m.design_id, 
-                count(t.in_production = false OR null) AS stock
-FROM modelos m
-JOIN transformadores t ON m.id_modelo = t.modelo_id_modelo 
-WHERE t.cliente = 'CDM'
-GROUP BY m.id_modelo""", nativeQuery = true
+        value =
+        """
+            SELECT m.id_modelo, m.cantidad_aceite, m.fase, m.kva, m.nombre_modelo, m.peso, m.tipo_aceite, m.tipo_transformador, m.design_id,
+            count(t.in_production = false OR null) AS stock
+            FROM modelos m
+            JOIN transformadores t ON m.id_modelo = t.modelo_id_modelo 
+            WHERE t.cliente = 'CDM'
+            GROUP BY m.id_modelo
+            """,
+        nativeQuery = true
     )
     fun getAllWithStock(): List<Modelo>
 
